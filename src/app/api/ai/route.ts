455befa5103373a966de0a01637cfcaa269bc27c
Vini Fraga <vinifragam@gmail.com>
2025-03-10 11:57:21 -0300

    AI SDK - Utilizando Tool Calling

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,20 +1,46 @@
 import { openrouter } from "@/ai/open-router";
-import { generateObject } from "ai";
+import { generateText, tool } from "ai";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { z } from 'zod'
 
 export async function GET(request: NextRequest) {
-  const result = await generateObject({
+  const result = await generateText({
     model: openrouter.chat('openai/gpt-4o-2024-11-20'),
-    schema: z.object({
-      pt: z.string().describe("Tradução para português"),
-      fr: z.string().describe("Tradução para francês"),
-      es: z.string().describe("Tradução para espanhol"),
-    }),
-    prompt: 'Traduza "Hello World" para diferentes idiomas!',
-    system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
+    tools: {
+      profileAndUrls: tool({
+        description: 'Essa ferramenta serve para buscar do perfil de um usuário do GitHub ou acessar URLs da API para outras informações de um usuário como lista de organizações, repositórios, eventos, seguidores, seguindo, etc...',
+        parameters: z.object({
+          username: z.string().describe('Username do usuário no GitHub'),
+        }),
+        execute: async ({ username }) => {
+          const response = await fetch(`https://api.github.com/users/${username}`)
+          const data = await response.json()
+
+          return JSON.stringify(data)
+        }
+      }),
+
+      fetchHTTP: tool({
+        description: "Essa ferramenta serve para realizar uma requisição HTTP em uma URL especificada e acessar sua resposta",
+        parameters: z.object({
+          url: z.string().describe('URL a ser requisitada'),
+        }),
+        execute: async ({ url }) => {
+          const response = await fetch(url)
+          const data = await response.text()
+
+          return data
+        }
+      })
+    },
+    prompt: 'Me dê uma lista de usuários que o usuário diego3g segue no GitHub?',
+    maxSteps: 5,
+
+    onStepFinish({ toolResults }) {
+      console.log(toolResults)
+    }
   })
 
-  return NextResponse.json({ message: result.object })
+  return NextResponse.json({ message: result.text, parts: result.toolResults })
 }
