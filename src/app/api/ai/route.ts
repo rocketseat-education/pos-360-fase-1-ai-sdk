2cefe3a073cbe6a00ce343a3a82e55552ce093b5
Vini Fraga <vinifragam@gmail.com>
2025-03-10 11:30:59 -0300

    AI SDK - Geração de dados estruturados

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,15 +1,20 @@
 import { openrouter } from "@/ai/open-router";
-import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
+import { generateObject } from "ai";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import { z } from 'zod'
 
 export async function GET(request: NextRequest) {
-  const result = await generateText({
-    model: openrouter.chat('openai/chatgpt-4o-latest'),
-    prompt: 'Traduza "Hello World" para português!',
+  const result = await generateObject({
+    model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+    schema: z.object({
+      pt: z.string().describe("Tradução para português"),
+      fr: z.string().describe("Tradução para francês"),
+      es: z.string().describe("Tradução para espanhol"),
+    }),
+    prompt: 'Traduza "Hello World" para diferentes idiomas!',
     system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
   })
 
-  return NextResponse.json({ message: result.text })
+  return NextResponse.json({ message: result.object })
 }
